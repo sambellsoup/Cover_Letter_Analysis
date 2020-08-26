68c35804ee19989f661eee833fc4860b14a0a49c
Sam Bell <sambellsoup@gmail.com>
2020-08-26 11:18:08 -0400

    updating settings to remove sensitive information

--- rookieplay/settings.py
+++ rookieplay/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1y%6yu_ofkz(q*cu+w!3@5nu%+o7x!7p=yi)wc%ifc!daepk3&'
+SECRET_KEY = key_secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
